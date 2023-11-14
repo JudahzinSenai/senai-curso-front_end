34d2cc02140a98dea7d9a1678d3995af9cda04bc
JudahzinSenai <145393781+JudahzinSenai@users.noreply.github.com>
2023-11-14 17:58:02 -0300

    Atividade de Bootstrap
    Vou organizar quando chegar em casa

--- CONTEÚDOS/3HTML E CSS/ATIVIDADES/H&C - Atividade 10/index.js
+++
@@ -1,9 +0,0 @@
-const menuMobile = document.querySelector(".menuMobile")
-
-function openMenuMobile(){
-    if (menuMobile.style.display == "none") {
-        menuMobile.style.display = "flex"
-    } else {
-        menuMobile.style.display = "none"
-    }
-}
